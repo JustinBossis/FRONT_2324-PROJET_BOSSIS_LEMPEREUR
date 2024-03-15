813591f96b7ab99510c82454cb9c9e9a66369a1d
Noemie-Lempereur <lempereur.noemie@gmail.com>
2024-03-15 16:46:34 +0100

    Add User details

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../../model/user';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent {
+  user: User = new User(
+    {
+      id: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      username: 'johndoe',
+      email: 'test@gmail.com',
+      admin: false,
+      favorites: [],
+      birthdate: "01/01/1990",
+      picture: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",
+    }
+  );
+
+  getFavoritesCount(): number {
+    return this.user.favorites.length;
+  }
+
+  viewFavorites(): void {
+    console.log('viewFavorites');
+  }
+
+  
 
 }
