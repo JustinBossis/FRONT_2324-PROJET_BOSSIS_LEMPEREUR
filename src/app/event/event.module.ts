062e86f4cf0de56147878c9ab26af1dc8a8843a6
Justin Bossis <bossisjustin@gmail.com>
2024-03-15 16:49:18 +0100

    Add navigation bar

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { EventRoutingModule } from './event-routing.module';
 
 
 
@@ -11,7 +13,12 @@ import { EventListComponent } from './event-list/event-list.component';
     EventListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    EventRoutingModule,
+    SharedModule,
+  ],
+  exports: [
+    EventListComponent
   ]
 })
 export class EventModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -12,12 +13,14 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
   ],
   imports: [
     FormsModule,
+    AppRoutingModule,
     CommonModule
   ],
   exports: [
-    FormsModule,
+    FormsModule,  
     CommonModule,
-    HttpClientModule
+    NavigationBarComponent,
+    HttpClientModule,
   ]
 })
 export class SharedModule { }
