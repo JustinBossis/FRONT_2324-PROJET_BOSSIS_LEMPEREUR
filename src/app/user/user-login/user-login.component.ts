697d1eca37363f74c3bce17ae0c11284672d9c8c
Noemie-Lempereur <lempereur.noemie@gmail.com>
2024-03-19 10:06:46 +0100

    submit register

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-user-login',
@@ -6,13 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
-
+  newUser: User = new User(
+    {
+      id: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      username: 'johndoe',
+      email: 'test@gmail.com',
+      admin: false,
+      favorites: [],
+      birthdate: "01/01/1990",
+      picture: "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",
+    }
+  );
   isLogin: boolean = true;
 
   constructor() { }
 
   changeIsLogin(): void {
     this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(registerForm:any): void {
+    console.log(this.newUser);
   }
 
 }
